a8e03770c38722bb1e198c7f4bb1129e3440d9d0
Kate <you@example.com>
2020-09-17 15:58:56 +0300

    services search files updated

--- src/app/github-service.service.ts
+++ src/app/github-service.service.ts
@@ -12,26 +12,34 @@ export class GithubServiceService {
 
   githubs:Github[]=[];
 
-  constructor(
-    private http: HttpClient) {
-      searchGithub(searchbar:String) {
-        interface data {
-          login:string;
-          username:string;
-          avatar:string;
-          html_url:string;
-          name:string;
-        }
-        let promise=new Promise((resolve, reject)=>{
-          this.http.get<data>('https://api.github.com/users/' + searchbar + '?access_token=' + environment.apiKey).toPromise().then(
-            (results) => {
-              this.githubs
-            }
-          )
-          
-          
-          })
-        })
+  constructor(private http: HttpClient) {}
+
+    searchGithub(searchbar:String) {
+      interface data {
+        login:string;
+        username:string;
+        avatar:string;
+        html_url:string;
+        name:string;
       }
-     }
+      let promise=new Promise((resolve, reject)=>{
+        this.http.get<data>('https://api.github.com/users/' + searchbar + '?access_token=' + environment.apiKey).toPromise().then(
+          (results) => {
+            this.githubs = [];
+            this.githubs.push(results);
+            console.log (results)
+            resolve()
+          },
+          (error) => {
+            console.log(error)
+            reject()
+          }
+          
+      )
+        
+        
+        })
+      })
+    }
 }
+    
