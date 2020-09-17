15508593ed01b53b73cccade2c9d9c4cb71622a5
Kate <you@example.com>
2020-09-17 14:51:06 +0300

    services created

--- src/app/github-service.service.ts
+++ src/app/github-service.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { User } from './user';
+import { Github } from './github';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubServiceService {
 
-  constructor() { }
+  githubs:Github[]=[];
+
+  constructor(
+    private http: HttpClient) {
+      searchGithub(searchbar)
+     }
 }
