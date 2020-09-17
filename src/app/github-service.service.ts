7bb69ef43bb00ae1e35b1b94d23be6f62992eedf
Kate <you@example.com>
2020-09-17 16:07:27 +0300

    promise returned

--- src/app/github-service.service.ts
+++ src/app/github-service.service.ts
@@ -14,32 +14,30 @@ export class GithubServiceService {
 
   constructor(private http: HttpClient) {}
 
-    searchGithub(searchbar:String) {
-      interface data {
-        login:string;
-        username:string;
-        avatar:string;
-        html_url:string;
-        name:string;
-      }
-      let promise=new Promise((resolve, reject)=>{
-        this.http.get<data>('https://api.github.com/users/' + searchbar + '?access_token=' + environment.apiKey).toPromise().then(
-          (results) => {
-            this.githubs = [];
-            this.githubs.push(results);
-            console.log (results)
-            resolve()
-          },
-          (error) => {
-            console.log(error)
-            reject()
-          }
-          
-      )
-        
-        
-        })
-      })
+  searchGithub(searchbar:String) {
+    interface data {
+      login:string;
+      username:string;
+      avatar:string;
+      html_url:string;
+      name:string;
     }
-}
+    let promise=new Promise((resolve, reject)=>{
+      this.http.get<data>('https://api.github.com/users/' + searchbar + '?access_token=' + environment.apiKey).toPromise().then(
+        (results) => {
+          this.githubs = [];
+          this.githubs.push(results);
+          console.log (results)
+          resolve()
+        },
+        (error) => {
+          console.log(error)
+          reject()
+        }
+      )      
+    })
+      return promise;
+  }
+  }
+
     
